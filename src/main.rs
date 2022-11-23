c28ec181e6f879498dfa41e1006e6a6b86f1d69d
Tomás Apolónia <75183079+tomasApo@users.noreply.github.com>
2022-11-23 13:47:10 +0000

    modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,45 @@
 #![no_main]
 #![no_std]
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
-use panic_rtt_target as _;
+
 use stm32f3xx_hal as hal;
+
+use cortex_m_rt::entry;
 use hal::pac;
 use hal::prelude::*;
-use hal::pwm::{tim3,tim16};
-use hal::time::rate::*;
-
-
+use panic_semihosting as _;
 
 
 #[entry]
 fn main() -> ! {
-  
-
-
-
-    //set the resolution of our duty cycle to 9000 
-    //period to 50Hz
-
-    // Set the resolution of our duty cycle to 9000 and our period to
-    // 50Hz.
-
     let dp = pac::Peripherals::take().unwrap();
 
-    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
 
-    let mut rcc = dp.RCC.constrain();
-    
-    let clock = rcc.cfgr.freeze(&mut flash.acr);
-  
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-  
-    // Set the resolution of our duty cycle to 9000 and our period to
-    // 50Hz.
-    let mut c1_no_pins = tim16(device.TIM3, 9000, 50.Hz(), clocks);
+    let mut led = gpioe
+        .pe13
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    led.set_low().unwrap();
+
     loop {
-          asm::delay(8_000_000);
+        led.toggle().unwrap();
+        cortex_m::asm::delay(1_000_000);
+
+        
+        // Toggle by hand.
+        // Uses `StatefulOutputPin` instead of `ToggleableOutputPin`.
+        // Logically it is the same.
+        
+        /*
+        if led.is_set_low().unwrap() {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().unwrap();
+        }
+        cortex_m::asm::delay(1_000_000);
+         */
     }
 }   
  
