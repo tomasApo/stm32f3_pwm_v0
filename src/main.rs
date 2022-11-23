75f50a650bcbe0662d1e97133eeb7d6e68828ca3
Tomás Apolónia <75183079+tomasApo@users.noreply.github.com>
2022-11-23 20:48:05 +0000

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@
 //cuse cortex_m::asm;
 use cortex_m_rt::entry;
 use panic_halt as _;
+
 use stm32f3xx_hal as hal;
+
 use hal::pac;
 use hal::flash::FlashExt;
 use hal::time::U32Ext;
@@ -37,13 +39,11 @@ fn main() -> ! {
 
         //tim3_ch1.get_max_duty() = 1280
         
-        //cortex_m::asm::delay(5_000_000);
-        
         loop {
             tim3_ch1.set_duty(tim3_ch1.get_max_duty()/10); // 5% duty cyle
             tim3_ch1.enable();
             cortex_m::asm::delay(5_000_000);
-            tim3_ch1.set_duty(tim3_ch1.get_max_duty()/20); // 5% duty cyle
+            tim3_ch1.set_duty(tim3_ch1.get_max_duty()/20); // 10% duty cyle
             tim3_ch1.enable();
             cortex_m::asm::delay(5_000_000);
             //rtt_init_print!();
