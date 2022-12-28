43e525412150339ae1d96a8529229622c27cec63
Tomás Apolónia <75183079+tomasApo@users.noreply.github.com>
2022-12-28 19:17:42 +0000

    not working but good progress

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@
 use cortex_m_rt::entry;
 use panic_halt as _;
 
-use stm32f3xx_hal as hal;
-
+//use stm32f3xx_hal as hal;
+use stm32f3::stm32f303;
+/*
 use hal::pac;
 use hal::flash::FlashExt;
 use hal::time::U32Ext;
@@ -14,40 +15,55 @@ use hal::pwm::tim3;
 use hal::hal::PwmPin;
 use hal::gpio::GpioExt;
 use hal::rcc::RccExt;
+*/
 
 #[entry]
 fn main() -> ! {
-        let dp = pac::Peripherals::take().unwrap();
-        use rtt_target::{rprintln, rtt_init_print};
-        let mut flash = dp.FLASH.constrain();
-        let mut rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.freeze(&mut flash.acr);
+        
+        
+        //let dp = pac::Peripherals::take().unwrap();
+        
+        use rtt_target::{rprintln};
+        
+        let peripherals = stm32f303::Peripherals::take().unwrap();
+        let mut flash = peripherals.FLASH;
+        let mut rcc = peripherals.RCC;
+        let clocks = &rcc.cfgr;
+        
+        
+        let mut gpioa = &peripherals.GPIOA(&mut flash.acr);
+        gpioa.odr.modify(|_, w| w.odr0().set_bit());
+        let pa6 = gpioa;
 
-        let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-        //let pa4 = gpioa.pa4.into_af2(&mut gpioa.moder, &mut gpioa.afrl);
-        let pa6 = gpioa.pa6.into_af2(&mut gpioa.moder, &mut gpioa.afrl);
 
-        let tim3_channels = tim3(
-            dp.TIM3,
-            1280,    // resolution of duty cycle
-            50.hz(), // frequency of period
-            &clocks, // To get the timer's clock speed
-        );
-
+        //clocks 
+        //let mut flash = dp.FLASH.constrain();
+        
+    
+        
+        peripherals.TIM3.ccmr1_output();
+        let tim3_ch1 = &peripherals.TIM3;
+        tim3_ch1.ccmr1_output();
+        
+        let mut tim3_ch1 = 
+        
+        
+        
         // Each channel can be used with a different duty cycle and have many pins
-        let mut tim3_ch1 = tim3_channels.0.output_to_pa6(pa6);
+        //let mut tim3_ch1 = tim3.ccmr1_output(pa6);
 
         //tim3_ch1.get_max_duty() = 1280
         
         loop {
+            /*
             tim3_ch1.set_duty(tim3_ch1.get_max_duty()/10); // 5% duty cyle
             tim3_ch1.enable();
             cortex_m::asm::delay(5_000_000);
             tim3_ch1.set_duty(tim3_ch1.get_max_duty()/20); // 10% duty cyle
             tim3_ch1.enable();
             cortex_m::asm::delay(5_000_000);
+             */
             //rtt_init_print!();
-            //rprintln!("{}",tim3_ch1.get_max_duty());
-            
-        }     
-}
+            rprintln!("{:?}",tim3_ch1);
+        };    
+}   
